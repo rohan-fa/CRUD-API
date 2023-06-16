ed7b3f363e8f0eb8b4662dd8d7ced952d2136b53
rohan-fa <rohanfarzana@gmail..com>
2023-06-16 18:32:34 +1000

    setting up express

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {express} from 'express';
-import { bodyParser } from 'body-parser';  //allow us to take in incoming post request bodies 
+import express from "express";
+import bodyParser from "body-parser";  //allow us to take in incoming post request bodies 
 
 //initialize our application:
 
@@ -8,7 +8,11 @@ const PORT = 5000;
 
 // bodyparser middlewire
 
-app.use(bodyParser.json())  //this is say we are going to use json data in our whole application. 
+app.use(bodyParser.json())  //this is say we are going to use json data in our whole application.
+app.get('/', (req, res) => {
+    console.log('test');
+    res.send('hellO')
+}); 
 
 //we can add listen to our incoming request
-app.listen(PORT,() => console.log('server running'))
+app.listen(PORT, () =>console.log(`Server running on port at http://localhost:${PORT}`));
--- steps.md
+++ steps.md
@@ -1,4 +1,11 @@
-* make a index.js folder
- run 
+# index.js 
+run 
  npm install --save express
-
+- go to package.json, after main write "type": "module" (it will allow us to import)
+###  After running the server error showed
+- for that we need to give the routing 
+### lets create our first route
+-after that lets install nodemon 
+npm install --save-dev nodemon
+### package.json
+- remove the "test" and replace that with "start": "nodemon index.js"
